e13bd55d8755ab232de06afc0b38a31a4e902968
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2024-12-26 15:40:30 +0800

    add some error handling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from getpass import getpass
-import time
+
+from output import output
 # import keyring
 
 # using HTTP so the captive portal can intercept with the connection
@@ -15,20 +17,26 @@ pasword = getpass()
 browser = webdriver.Safari()
 
 browser.get(test_link)
-print(prefix, "Success")
+if browser.current_url == test_link:
+    output("Redirection failed. Please check your network connection or check if you're already logged-in")
+else:
+    print(prefix, "Successfully redirect to", browser.current_url)
+    try:
+        web_username = browser.find_element(By.NAME, "username")
+        web_password = browser.find_element(By.NAME, "password")
+    except NoSuchElementException:
+        print(prefix, "ERROR:", "Element not found")
+    else:
+        print(prefix, r"Successfully located the element 'username' and 'password' ")
 
-time.sleep(5)
+        # pass the data in
+        web_username.send_keys(username)
+        web_password.send_keys(pasword)
+        # enter to trigger login
+        web_password.send_keys(Keys.ENTER)
 
-web_username = browser.find_element(By.NAME, "username")
-web_password = browser.find_element(By.NAME, "password")
 
-print(prefix, "Success input data")
-
-web_username.send_keys(username)
-web_password.send_keys(pasword)
-
-web_password.send_keys(Keys.ENTER)
-
+    
 
 
 browser.quit()
